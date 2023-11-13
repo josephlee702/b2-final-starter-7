69fe89634b25a6170b727d6e8593fca3eb88cdfd
Joseph Lee <jhjlee702@gmail.com>
2023-11-12 23:33:49 -0700

    finish US2 and testing

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -6,4 +6,26 @@ class BulkDiscountsController < ApplicationController
   def show
     @bulk_discount = BulkDiscount.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @new_bulk_discount = @merchant.bulk_discounts.new(bulk_discount_params)
+
+    if @new_bulk_discount.save
+      redirect_to "/merchants/#{@merchant.id}/bulk_discounts"
+      flash[:notice] = "This bulk discount was successfully saved!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bulk_discount_params
+    params.permit(:name, :percentage_discount, :quantity_threshold)
+  end
 end
